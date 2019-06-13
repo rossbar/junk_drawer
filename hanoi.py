aa25fb94fe42ebeb25cec9da88a00a1fc8733497
Ross Barnowski <rossbar@berkeley.edu>
2019-06-13 12:52:29 -0700

    Added recursive solution to towers of hanoi.
    See CCSPiP sec 1.22

--- hanoi.py
+++ hanoi.py
@@ -14,3 +14,26 @@ class Stack(list):
 
     def push(self, item):
         self.append(item)
+
+def solve(begin, end, intermediary, disc):
+    """
+    Recursive solution to Towers of Hanoi.
+
+    Parameters
+    ----------
+    begin :        Stack
+        Initial tower on which the discs are located.
+    end   :        Stack
+        Empty tower to which the discs are to be transported
+    intermediary : Stack
+        Empty tower to be used as intermediary for disc transfer
+    disc :         int
+        Disc number
+    """
+    print(begin, intermediary, end, '\n')
+    if disc == 1:
+        end.push(begin.pop())
+    else:
+        solve(begin, intermediary, end, disc - 1)
+        solve(begin, end, intermediary, 1)
+        solve(intermediary, end, begin, disc - 1)
