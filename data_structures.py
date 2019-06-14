badea8fa11e44da6b885a7e3318da9141e23bbf7
Ross Barnowski <rossbar@berkeley.edu>
2019-06-13 21:07:14 -0700

    Moved Stack from hanoi.py to data_structures.py
    Will be reusing the stack for depth-first search in maze.py, so
    moved to a new module so it can be reused.
    
    hanoi.py modified to import Stack from its new module.

---
+++ data_structures.py
@@ -0,0 +1,16 @@
+"""
+Some data structures not included in collections
+"""
+
+class Stack(list):
+    """
+    A stack derived from the python list.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return '\n'.join(str(val) for val in self)
+
+    def push(self, item):
+        self.append(item)
--- hanoi.py
+++ hanoi.py
@@ -1,19 +1,7 @@
 """
 Towers of Hanoi
 """
-
-class Stack(list):
-    """
-    A stack derived from the python list.
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def __repr__(self):
-        return '\n'.join(str(val) for val in self)
-
-    def push(self, item):
-        self.append(item)
+from data_structures import Stack
 
 class TowersOfHanoi(object):
     """
